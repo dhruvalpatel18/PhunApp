a3b0854c855d0a2741539291b9f47a61e326bf3b
dhruval <dhruval.18@gmail.com>
2016-11-17 14:36:58 -0800

    Initial commit

---
+++ app/src/main/java/com/handm/dhruval/phunapp/HomeLanding.java
@@ -0,0 +1,13 @@
+package com.handm.dhruval.phunapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeLanding extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_landing);
+    }
+}

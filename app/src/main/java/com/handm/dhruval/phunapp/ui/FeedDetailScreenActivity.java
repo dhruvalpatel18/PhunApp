592a9f1dd8f81a4cc264f54d5c9b37a3c8383206
dhruval <dhruval.18@gmail.com>
2016-11-17 19:07:07 -0800

    Add logic for menu item select

--- app/src/main/java/com/handm/dhruval/phunapp/ui/FeedDetailScreenActivity.java
+++ app/src/main/java/com/handm/dhruval/phunapp/ui/FeedDetailScreenActivity.java
@@ -3,14 +3,19 @@ package com.handm.dhruval.phunapp.ui;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.Window;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.handm.dhruval.phunapp.R;
+import com.handm.dhruval.phunapp.helper.FeedSharingHelper;
 import com.handm.dhruval.phunapp.model.CardInfo;
 import com.squareup.picasso.Picasso;
 
@@ -77,5 +82,32 @@ public class FeedDetailScreenActivity extends AppCompatActivity {
 
         String formattedDate = new SimpleDateFormat(EXPECTED_DATE_FORMAT).format(date);
         return formattedDate.replace("AM", "am").replace("PM", "pm");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_call:
+                if (mPhoneNum == null || mPhoneNum.isEmpty()) {
+                    Toast.makeText(this, "Phone number not mentioned", Toast.LENGTH_LONG).show();
+                } else {
+                    Intent callIntent = new Intent(Intent.ACTION_DIAL);
+                    callIntent.setData(Uri.parse("tel:" + mPhoneNum));
+                    startActivity(callIntent);
+                }
+                return true;
+            case R.id.action_share:
+                FeedSharingHelper feedSharingHelper = new FeedSharingHelper(this);
+                feedSharingHelper.shareClickAction(description.getText().toString());
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }

382d4a2080d17cc2f8754faf8f52948edeb98e5e
dhruval <dhruval.18@gmail.com>
2016-11-17 17:54:06 -0800

    Add DetailScreenActivity and layout

---
+++ app/src/main/java/com/handm/dhruval/phunapp/ui/FeedDetailScreenActivity.java
@@ -0,0 +1,15 @@
+package com.handm.dhruval.phunapp.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.handm.dhruval.phunapp.R;
+
+public class FeedDetailScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed_detail_screen);
+    }
+}

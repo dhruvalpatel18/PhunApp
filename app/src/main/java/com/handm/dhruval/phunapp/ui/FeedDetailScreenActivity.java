b8eb933642a3b789f9ab1f825239087ed03ac34e
dhruval <dhruval.18@gmail.com>
2016-11-17 18:40:32 -0800

    Get detail data and set to FeedDetailScreenActivity

--- app/src/main/java/com/handm/dhruval/phunapp/ui/FeedDetailScreenActivity.java
+++ app/src/main/java/com/handm/dhruval/phunapp/ui/FeedDetailScreenActivity.java
@@ -1,15 +1,70 @@
 package com.handm.dhruval.phunapp.ui;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.handm.dhruval.phunapp.R;
+import com.handm.dhruval.phunapp.model.CardInfo;
+import com.squareup.picasso.Picasso;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class FeedDetailScreenActivity extends AppCompatActivity {
+    private static final String UTC_DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSSZ";
+    private static final String EXPECTED_DATE_FORMAT = "MMM dd, yyyy 'at' HH:mma";
+
+    TextView title, time, description;
+    ImageView imageView;
+    String mPhoneNum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed_detail_screen);
+
+        initView();
+        setViewContents();
+    }
+
+    private void initView() {
+        title = (TextView)findViewById(R.id.detail_title);
+        time = (TextView)findViewById(R.id.detail_time);
+        imageView = (ImageView)findViewById(R.id.imageView);
+        description = (TextView)findViewById(R.id.detail_description);
+    }
+
+    private void setViewContents() {
+        Intent intent = getIntent();
+
+        parseDate(intent.getStringExtra(CardInfo.JSON_TIMESTAMP));
+
+        title.setText(intent.getStringExtra(CardInfo.JSON_TITLE));
+        time.setText(parseDate(intent.getStringExtra(CardInfo.JSON_TIMESTAMP)));
+        description.setText(intent.getStringExtra(CardInfo.JSON_DESCRIPTION));
+        mPhoneNum = intent.getStringExtra(CardInfo.JSON_PHONE);
+
+        String detailImageURL = intent.getStringExtra(CardInfo.JSON_IMAGE);
+        Picasso.with(getApplicationContext())
+                .load(detailImageURL)
+                .placeholder(R.drawable.placeholder_nomoon)
+                .error(R.drawable.placeholder_nomoon)
+                .into(imageView);
+    }
+
+    public static String  parseDate(String dateTimeStr) {
+        Date date = new Date();
+        try {
+            date = (new SimpleDateFormat(UTC_DATE_FORMAT)).parse(dateTimeStr.replaceAll("Z$", "+0000"));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        String formattedDate = new SimpleDateFormat(EXPECTED_DATE_FORMAT).format(date);
+        return formattedDate.replace("AM", "am").replace("PM", "pm");
     }
 }
